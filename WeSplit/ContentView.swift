4b3e08a742f7b6110302a5b941c2bb6dfc9657ac
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2022-06-22 17:25:47 +0100

    added a "Done" button that appears above the decimal keyboard and along with the decimal keyboard and hides the keyboard when presssed.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,128 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @State var checkAmount: Double = 0.0
+    @State var numberOfPeople = 2
+    @State var tipPercentage = 20
+    @FocusState var amountIsFocused: Bool
+
+    
+     let tipPercentages = [0, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        
+        let tipValue =  checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+        }
+    
+    var tippedValue: Double {
+        let a = Double(numberOfPeople + 2)
+        let b = checkAmount
+        let c = totalPerPerson
+                     
+        let amountTipped = c * a - b
+            
+
+        return amountTipped
+        
     }
-}
+        
+        
+        var totalAmount: Double {
+          let toBePaid = checkAmount + tippedValue
+            
+            return toBePaid
+        }
+
+    
+        
+    
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                
+                Section {
+                    TextField("The cost of the bill is \(checkAmount)", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD" ))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker(selection: $numberOfPeople, label: Text("Number of People")) {
+                         
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    
+                   
+                        }
+                   
+                    }
+                
+                Section {
+                    Picker(selection: $tipPercentage, label: Text("Tip Percentage")) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                            
+                        }
+                    .pickerStyle(.segmented)
+                    }header: {
+                        Text("How much would you like to tip ")
+                    }
+                
+                Section {
+                   Text(tippedValue, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }header: {
+                    Text("a \(tipPercentage, format: .percent) tip is  ")
+                }
+  
+                    
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD" ))
+                }header: {
+                    Text("\(numberOfPeople + 2) of you will contribute \(totalPerPerson,  format: .currency(code: Locale.current.currencyCode ?? "USD" )) each")
+                }
+                
+               
+                
+                Section {
+                   Text(totalAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }header: {
+                    Text("The the total amount ")
+                }
+    
+            }
+            .navigationTitle("Tip Calculator")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+
+        }
+
+
+        
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+    
+
+}
+
+
+
